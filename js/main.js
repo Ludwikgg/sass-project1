62497ba756a49f893c461052879cae8ff6d0d478
Damian <damian.ludwig83@gmail.com>
2021-10-28 17:47:59 +0200

    created and styled navigation

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const nav = document.querySelector('.nav__wrapper');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__item');
+
+const handleNav = () => {
+    nav.classList.toggle('nav__wrapper--active');
+
+    allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+            nav.classList.remove('nav__wrapper--active')
+        })
+    })
+
+    handleNavItemsAnimation();
+}
+
+const handleNavItemsAnimation = () => {
+    let delayTime = 0;
+
+    allNavItems.forEach(item => {
+        item.classList.toggle('nav-items-animation');
+        item.style.animationDelay = '.' + delayTime + 's';
+        delayTime++;
+    })
+}
+
+navBtn.addEventListener('click', handleNav)
